b54481a5f609c372676574b2f141db422c3daf2a
Patricia Steinberg <patriciacsteinberg@gmail.com>
2019-06-16 16:04:10 -0300

    Creation of feed and new post routes using React Framework

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+import logo from '../assets/logo.svg';
+import camera from '../assets/camera.svg';
+
+export default function Header() {
+  return (
+    <header id="main-header">
+        <div className="header-content">
+            <img src="" alt="InstaReplica" />
+            <img src="" alt="Enviar publicação" />
+        </div>
+    </header>
+  );
+}
