840359f855389912bd5364dc45ca4cfa0a78fb3f
Patricia Steinberg <patriciacsteinberg@gmail.com>
2019-06-23 10:53:31 -0300

    Creation of Feed.css and Header.css

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import './Feed.css';
+import more from '../assets/more.svg';
+import like from '../assets/like.svg';
+import comment from '../assets/comment.svg';
+import send from '../assets/send.svg';
 
 class Feed extends Component {
     render () {
         return (
-            <h1>Feed</h1>
+            <section id='post-list'>
+                <article>
+                    <header>
+                        <div className='user-info'>
+                            <span>Patricia Steinberg</span>
+                            <span className='place'> Rio de Janeiro </span>
+                        </div>
+
+                        <img src={more} alt="Mais" />
+                    </header>
+                </article>
+            </section>
         );
     }
 }
